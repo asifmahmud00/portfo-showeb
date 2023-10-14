945bd96ea811c43d665d00a79fda4688d92a63ef
asifmahmud00 <asif.mahmud00@gmail.com>
2023-10-14 23:16:33 +0600

    Project Biztech 1st commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+
+import './App.css'
+
+
+function App() {
+
+
+
+
+  return (
+    <>
+      <button className="btn">Bismillah</button>
+      <button className="btn btn-primary">Subhanallah</button>
+      <button className="btn btn-secondary">Alhamdulillah</button>
+      <button className="btn btn-accent">La Ilaha Illallah</button>
+      <button className="btn btn-ghost">Allhu Akbar</button>
+      <button className="btn">La Hawla Wala Quuata Illabillah</button>
+      <button className="btn btn-link">Wa lillahil Hamd</button>
+    </>
+
+
+  )
+}
+
+
+export default App
